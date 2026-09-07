89faa7db332231c1842b767df46a9437b420551b
lil <natavxz@gmail.com>
2026-09-07 15:01:01 -0400

    Create embeds.py

---
+++ utils/embeds.py
@@ -0,0 +1,10 @@
+import discord
+
+
+def embed_error(mensaje: str) -> discord.Embed:
+    return discord.Embed(description=f"❌ {mensaje}", color=discord.Color.red())
+
+
+def embed_exito(mensaje: str) -> discord.Embed:
+    return discord.Embed(description=f"✅ {mensaje}", color=discord.Color.green())
+  
